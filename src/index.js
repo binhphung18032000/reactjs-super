243aa4f2e11d7515ed8921d5abc30b1375608891
Binh Phung <binhphung18032000@gmail.com>
2025-06-22 23:21:02 +0700

    [REACT-JS] Render element

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+// example 1:
+// const element = <h1>Hello anh em!</h1>
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(element)
+
+// example 2:
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function tick () {
+  const element = (
+    <div>
+      <h1>Xin chào anh chị em ^^</h1>
+      <h2>Bây giờ là {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+  root.render(element)
+}
+setInterval(tick, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
